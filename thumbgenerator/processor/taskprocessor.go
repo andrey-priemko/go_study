5cdce748bc8acaeaedb8ced94af1eaa0b7601656
andrey.priemko <andrey.priemko@ispringsolutions.com>
2019-06-19 18:49:07 +0300

    Обработка таска вынесена в отдельный модуль

---
+++ thumbgenerator/processor/taskprocessor.go
@@ -0,0 +1,39 @@
+package processor
+
+import (
+	"database/sql"
+	log "github.com/sirupsen/logrus"
+	"go_study/thumbgenerator/model"
+	"go_study/videoserver/database"
+	"go_study/videoserver/storage"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+)
+
+func ProcessTask(task *model.Task, db *sql.DB) {
+	thumbUrl := filepath.Join(filepath.Dir(task.Url), storage.ThumbFileName)
+	out, err := exec.Command("D:\\projects\\go_dev\\dev\\src\\go_study\\bin\\VideoProcessor.exe", task.Url, thumbUrl).Output()
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+
+	duration, err := strconv.ParseFloat(string(out), 64)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+
+	err = database.ExecTransaction(
+		db,
+		"UPDATE video SET status=?, duration=?, thumbnail_url=? WHERE video_key=?",
+		model.Processed,
+		duration,
+		thumbUrl,
+		task.VideoKey,
+	)
+	if err != nil {
+		log.Error(err.Error())
+	}
+}
--- thumbgenerator/worker/worker.go
+++ thumbgenerator/worker/worker.go
@@ -4,44 +4,14 @@ import (
 	"database/sql"
 	log "github.com/sirupsen/logrus"
 	"go_study/thumbgenerator/model"
-	"go_study/videoserver/database"
-	"go_study/videoserver/storage"
-	"os/exec"
-	"path/filepath"
-	"strconv"
+	"go_study/thumbgenerator/processor"
 )
 
 func Worker(tasksChan <-chan *model.Task, db *sql.DB, name int) {
 	log.Printf("start worker %v\n", name)
 	for task := range tasksChan {
 		log.Printf("start handle task %v on worker %v\n", task.VideoKey, name)
-
-		thumbUrl := filepath.Join(filepath.Dir(task.Url), storage.ThumbFileName)
-		out, err := exec.Command("D:\\projects\\go_dev\\dev\\src\\go_study\\bin\\VideoProcessor.exe", task.Url, thumbUrl).Output()
-		if err != nil {
-			log.Error(err.Error())
-			continue
-		}
-
-		duration, err := strconv.ParseFloat(string(out), 64)
-		if err != nil {
-			log.Error(err.Error())
-			continue
-		}
-
-		err = database.ExecTransaction(
-			db,
-			"UPDATE video SET status=?, duration=?, thumbnail_url=? WHERE video_key=?",
-			model.Processed,
-			duration,
-			thumbUrl,
-			task.VideoKey,
-		)
-		if err != nil {
-			log.Error(err.Error())
-			continue
-		}
-
+		processor.ProcessTask(task, db)
 		log.Printf("end handle task %v on worker %v\n", task.VideoKey, name)
 	}
 	log.Printf("stop worker %v\n", name)
